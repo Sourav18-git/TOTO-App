1028a4d16af2b5c56dd4d3f137376328509bbc9b
Sourav <149318575+Sourav18-git@users.noreply.github.com>
2023-12-02 19:51:21 +0530

    updated assets

---
+++ assets/data/types.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: '0',
+    type: 'TotoX',
+    price: 'Rs 220',
+    duration: 45,
+  },
+  {
+    id: '1',
+    type: 'Comfort',
+    price:'Rs 270',
+    duration: 30,
+  },
+  {
+    id: '2',
+    type: 'TotoXL',
+    price: 'Rs 360',
+    duration: 27,
+  },
+]
